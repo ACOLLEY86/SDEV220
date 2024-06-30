76f99be8f67c7dd7c9ac96f1f2e0d81b058435da
ACOLLEY86 <149801761+ACOLLEY86@users.noreply.github.com>
2024-06-30 19:44:39 -0400

    a

--- application.py
+++ application.py
@@ -1,6 +1,69 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/'):
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"{self.book_name} by {self.author} - {self.publisher}"
+
+@app.route('/')
 def index():
     return 'Hello!'
+
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'book_name': book.book_name,
+            'author': book.author,
+            'publisher': book.publisher
+        }
+        output.append(book_data)
+    return {"books": output}
+
+@app.route('/books/<int:id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {
+        'id': book.id,
+        'book_name': book.book_name,
+        'author': book.author,
+        'publisher': book.publisher
+    }
+
+@app.route('/books', methods=['POST'])
+def add_book():
+    data = request.get_json()
+    new_book = Book(
+        book_name=data['book_name'],
+        author=data['author'],
+        publisher=data['publisher']
+    )
+    db.session.add(new_book)
+    db.session.commit()
+    return {'id': new_book.id}, 201
+
+@app.route('/books/<int:id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book is None:
+        return {"error": "not found"}, 404
+    db.session.delete(book)
+    db.session.commit()
+    return {"message": "Book deleted!"}, 200
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
